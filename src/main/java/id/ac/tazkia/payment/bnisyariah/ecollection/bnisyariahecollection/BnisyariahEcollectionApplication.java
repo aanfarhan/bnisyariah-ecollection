777d93ba12d0761b5580d46ea373d4532d264f3b
Endy Muhardin <endy.muhardin@gmail.com>
2017-12-04 13:18:52 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/payment/bnisyariah/ecollection/bnisyariahecollection/BnisyariahEcollectionApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.payment.bnisyariah.ecollection.bnisyariahecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnisyariahEcollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnisyariahEcollectionApplication.class, args);
+	}
+}
