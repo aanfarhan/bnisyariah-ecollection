3b285c101105a87eb186872bc5d79f791cc054d2
Endy Muhardin <endy.muhardin@gmail.com>
2018-01-12 06:19:00 +0700

    enable pessimistic locking pada waktu generate running number

--- src/main/java/id/ac/tazkia/payment/bnisyariah/ecollection/dao/RunningNumberDao.java
+++ src/main/java/id/ac/tazkia/payment/bnisyariah/ecollection/dao/RunningNumberDao.java
@@ -1,8 +1,12 @@
 package id.ac.tazkia.payment.bnisyariah.ecollection.dao;
 
 import id.ac.tazkia.payment.bnisyariah.ecollection.entity.RunningNumber;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import javax.persistence.LockModeType;
+
 public interface RunningNumberDao extends PagingAndSortingRepository<RunningNumber, String> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     RunningNumber findByPrefix(String prefix);
 }
