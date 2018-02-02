78012e46eacd6a1958e4daed2c29861ac61bbbe9
Endy Muhardin <endy.muhardin@gmail.com>
2018-02-02 16:22:24 +0700

    index untuk mempercepat query

---
+++ src/main/resources/db/migration/V1.2.2.2018020201__Index_Column.sql
@@ -0,0 +1,3 @@
+create index invoice_number_idx on virtual_account(invoice_number);
+create index account_number_idx on virtual_account(account_number);
+create index account_status_idx on virtual_account(account_status);
